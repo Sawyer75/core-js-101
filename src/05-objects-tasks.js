90e19fbc21b6c1803c7e6a233edf27360b51bcbd
Oleg Krivenkov <krivenkof@gmail.com>
2023-07-02 16:31:00 +0300

    update js file

--- src/05-objects-tasks.js
+++ src/05-objects-tasks.js
@@ -33,10 +33,10 @@ function Rectangle(/* width, height */) {
  *    [1,2,3]   =>  '[1,2,3]'
  *    { width: 10, height : 20 } => '{"height":10,"width":20}'
  */
-function getJSON(/* obj */) {
-  throw new Error('Not implemented');
+function getJSON(obj) {
+  // throw new Error('Not implemented');
+  return JSON.stringify(obj);
 }
-
 /**
  * Returns the object of specified type from JSON representation
  *
@@ -48,8 +48,11 @@ function getJSON(/* obj */) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  // throw new Error('Not implemented');
+  const obj = JSON.parse(json);
+  const values = Object.values(obj);
+  return new proto.constructor(...values);
 }
 
 /**
